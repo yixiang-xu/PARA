fed82f380c36c86f40ff4542431ed27d47505f07
Yixiang Xu <104027457+yixiang-xu@users.noreply.github.com>
2024-11-18 19:48:11 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read(),  # Long description read from the readme file
+    long_description = f.read()  # Long description read from the readme file
         
 setup(
     name='para_paralanguage_classifier',
